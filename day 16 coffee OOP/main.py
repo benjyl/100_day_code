9f99355ed24bad228a8c7a0f7ec1110b2563394b
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-20 17:07:56 +0000

    initial commit

---
+++ day 16 coffee OOP/main.py
@@ -0,0 +1,22 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+on = True
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+while on:
+    order = input(f"What coffee would you like {menu.get_items()}? ")
+
+    if order == "off":
+        on = False
+    elif order == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        coffee = menu.find_drink(order)
+        if coffee:
+            if coffee_maker.is_resource_sufficient(coffee) and money_machine.make_payment(coffee.cost):
+                coffee_maker.make_coffee(coffee)
