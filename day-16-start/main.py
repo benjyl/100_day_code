e40467abf07e0fefca78e922f2344d100d05175b
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-20 17:08:56 +0000

    play with prettytable class

--- day-16-start/main.py
+++ day-16-start/main.py
@@ -1,12 +1,22 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 # import other_module
+
 
 # print(other_module.another_variable)
 
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("red")
-timmy.forward(100)
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+print(table.align)
+table.align = "l" # left aligned
+print(table)
