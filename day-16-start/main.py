b25005fba2cca28f312e0a5393dc001569df1b43
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-20 13:04:42 +0000

    finish turtle example

--- day-16-start/main.py
+++ day-16-start/main.py
@@ -1,1 +1,12 @@
-import turtle
+from turtle import Turtle, Screen
+# import other_module
+
+# print(other_module.another_variable)
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
