4d3bb650e73fa09487976a953790dbe9821d0d1b
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-21 18:27:42 +0000

    complete turtle race

--- day 19/turtle_race.py
+++ day 19/turtle_race.py
@@ -1,7 +1,39 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
 screen = Screen()
+screen.setup(width=500, height=400)
+race_on = False
+bet = screen.textinput(title="Make your Bet", prompt="Which colour turtle do you think will win")
+colours = ["red", "orange", "yellow", "green", "blue", "purple"]
 
+turtle_list = []
+y_pos = -100
+for colour in colours:
+    new_turt = Turtle(shape="turtle")
+    new_turt.color(colour)
+    new_turt.pu()
+    new_turt.goto(x=-230, y=y_pos)
+    y_pos += 40
+    turtle_list.append(new_turt)
+# print(turtle_list)
+
+if bet:
+    race_on = True
+
+while race_on:
+
+    for turtle in turtle_list:
+        rand_dist = random.randint(0,10)
+        turtle.forward(rand_dist)
+        if turtle.xcor() >=230:
+            winner = turtle.pencolor()
+            if winner == bet.lower():
+                print(f"You won! The winner was the {winner} turtle ")    
+            else:
+                print(f"You lost! The winner was the {winner} turtle ") 
+            race_on = False
+
+ 
 
 screen.exitonclick()
