1508212770d7a07d7f72d27b3a85e310a83588ba
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-21 10:57:33 +0000

    format code

--- day 17/day 17 start/main.py
+++ day 17/day 17 start/main.py
@@ -4,10 +4,11 @@ class User:
         self.username = username
         self.followers = 0
         self.following = 0
-        
+
     def follow(self, user):
         user.followers += 1
         self.following += 1
+
 
 user_1 = User("001", "Benjy")
 # print(user_1.username)
