936e1e0747488f3306b3e11987d915fa34e37b44
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-22 12:59:20 +0000

    initialise snake body

--- day 20-21 snake/main.py
+++ day 20-21 snake/main.py
@@ -3,3 +3,15 @@ from turtle import Screen, Turtle
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.title("SNAKE!!!!!!!!")
+initial_pos = [0, -20, -40]
+for i in range(3):
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.pu()
+    new_segment.goto(x=initial_pos[i], y=0)
+
+
+
+
+screen.exitonclick()
