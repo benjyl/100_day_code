b3a9a13036ac8b9f894246ef4a5cc3ca051b6da2
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-23 10:39:13 +0000

    initial commit

---
+++ day 20-21 snake/inheritance.py
@@ -0,0 +1,10 @@
+class Fish:
+    def __init__(self) -> None:
+        self.num_eyes = 2
+    
+    def breath(self):
+        print("inhale, exhale")
+
+class Human(Fish):
+    def __init__(self) -> None:
+        super().__init__()
