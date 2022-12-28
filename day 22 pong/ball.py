d80ab13d6fc2685491e8280d1c2c0b4ff506bdb9
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-28 11:49:44 +0000

    add ball attributes and move method

--- day 22 pong/ball.py
+++ day 22 pong/ball.py
@@ -1,7 +1,15 @@
 from turtle import Turtle
+import random
 
 
 class Ball(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.shape("circle")
+        self.ra
+        self.color("white")
+        self.pu()
+        self.setheading(random.randint(0, 355))
+        self.width()
+    def move(self):
+        self.fd(20)
