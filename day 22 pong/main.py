518d13bf7b774e48aae4ac77139cc773a7ba7b8b
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-28 11:19:58 +0000

    create paddle moving on screen click V1

--- day 22 pong/main.py
+++ day 22 pong/main.py
@@ -1,14 +1,15 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
+import time
 
-def create_centre():
+def create_centre(height):
     centre.color("white")
-    height = screen.window_height()
     centre.pu()
     centre.hideturtle()
     centre.pensize(8)
-    centre.goto(x=0, y=height/2)
+    centre.goto(x=0, y=height)
     screen.tracer(0) 
-    while centre.position()[1]> -height/2:
+    while centre.position()[1]> -height:
         centre.pd()
         centre.setheading(270)
         centre.forward(20)
@@ -17,9 +18,26 @@ def create_centre():
         
 screen = Screen()
 screen.setup(width=1.0, height=1.0) # float takes up whole screen
+s_h = screen.window_height()
+s_w = screen.window_width()
 screen.bgcolor("black")
+screen.title("Pong")
 centre = Turtle()
-create_centre()
-    
+create_centre(s_h/2)
+
+screen.listen()
+
+
+right_paddle = Paddle(s_w, s_w/2-50)
+
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
+screen.update()
+
+game_on = True
+
+while game_on:
+    screen.update()
+    time.sleep(0.1)
 
 screen.exitonclick()
--- day 22 pong/paddle.py
+++ day 22 pong/paddle.py
@@ -1,3 +1,24 @@
-class Paddle:
-    def __init__(self) -> None:
-        pass
+from turtle import Turtle
+
+UP = 90
+DOWN = 270
+MOVE_DIST = 20
+
+class Paddle(Turtle):
+    def __init__(self, screenwidth, x_pos) -> None:
+        super().__init__()
+        self.color("white")
+        self.pu()
+        self.resizemode("user")
+        self.shape("square")
+        self.setheading(90)
+        self.shapesize(stretch_len=5)
+        self.goto(x=x_pos, y=0)
+    
+    def up(self):
+        self.setheading(UP)
+        self.fd(MOVE_DIST)
+    
+    def down(self):
+        self.setheading(DOWN)
+        self.fd(MOVE_DIST)
