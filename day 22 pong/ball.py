6486e6058ec31d756633f5525bf390e9edbf4ba0
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-28 13:20:25 +0000

    add bounce method

--- day 22 pong/ball.py
+++ day 22 pong/ball.py
@@ -8,10 +8,23 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.pu()
-        # 
-        # self.setheading(random.randint(0, 355)) # if want to move randomly
-        # self.setheading
-        self.direct = np.arctan2(screen_y, screen_x) * 180/np.pi
-        self.setheading(self.direct)
-    def move(self):
-        self.fd(20)
+        self.x = 0
+        self.y = 0
+        self.x_inc = 10 # movement increment in x dir
+        self.y_inc = 10 # movement increment in y dir
+        
+        
+    # #set heading to make ball go in random direction when start
+        # self.setheading(random.randint(0, 355)) 
+    # set heading to go to top right corner every time
+        # self.direct = np.arctan2(screen_y, screen_x) * 180/np.pi
+        # self.setheading(self.direct)
+    
+    def move(self,screen_x, screen_y, ):
+        self.x += self.x_inc
+        self.y += self.y_inc
+        self.goto(self.x, self.y)
+    
+
+    def bounce(self):
+        self.y_inc = - self.y_inc            
