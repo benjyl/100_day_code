c09e128ca0d9b894a5536f8e70790f3e7ae2270b
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-28 12:29:24 +0000

    add setheading to go to top corner

--- day 22 pong/ball.py
+++ day 22 pong/ball.py
@@ -1,15 +1,17 @@
 from turtle import Turtle
 import random
-
+import numpy as np
 
 class Ball(Turtle):
-    def __init__(self) -> None:
+    def __init__(self, screen_x, screen_y) -> None:
         super().__init__()
         self.shape("circle")
-        self.ra
         self.color("white")
         self.pu()
-        self.setheading(random.randint(0, 355))
-        self.width()
+        # 
+        # self.setheading(random.randint(0, 355)) # if want to move randomly
+        # self.setheading
+        self.direct = np.arctan2(screen_y, screen_x) * 180/np.pi
+        self.setheading(self.direct)
     def move(self):
         self.fd(20)
