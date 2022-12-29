6366070ff0434a82b81dd36ee09eb4f5c1d7afed
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-29 14:38:39 +0000

    add attributes and move method

--- day 23 - Turtle crossing/player.py
+++ day 23 - Turtle crossing/player.py
@@ -1,7 +1,25 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.shape("turtle")
+        self.pu()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+        self.y = STARTING_POSITION[1]
+        self.level = 1
+    
+    def move(self):
+        """move player forward when up button hit
+        Check whether reached top of screen 
+        """
+        self.fd(MOVE_DISTANCE)
+        if self.ycor()>FINISH_LINE_Y:
+            self.goto(STARTING_POSITION)
+            self.level += 1
