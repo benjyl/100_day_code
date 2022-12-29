e72a8c055999a29e29310541a7ab96c96866f8d9
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-29 14:38:59 +0000

    add attributes and update method

--- day 23 - Turtle crossing/scoreboard.py
+++ day 23 - Turtle crossing/scoreboard.py
@@ -1,5 +1,17 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
-
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.pu()
+        self.ht() # hide turtle
+        
+        
+    def update_scoreboard(self, level):
+        self.clear()
+        self.goto(-150, 250)
+        self.write(f"Level {level}", align="center", font=FONT)
+        
+    
