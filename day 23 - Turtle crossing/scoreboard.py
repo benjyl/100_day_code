a40d918a5b3cb0679e2fb97029fd1bf710b01f17
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-29 17:07:16 +0000

    add update level and game over methods

--- day 23 - Turtle crossing/scoreboard.py
+++ day 23 - Turtle crossing/scoreboard.py
@@ -7,11 +7,18 @@ class Scoreboard(Turtle):
         super().__init__()
         self.pu()
         self.ht() # hide turtle
+        self.level = 1
+    
+    def update_level(self):
+        self.level += 1
+        self.update_scoreboard()
         
-        
-    def update_scoreboard(self, level):
+    def update_scoreboard(self):
         self.clear()
         self.goto(-150, 250)
-        self.write(f"Level {level}", align="center", font=FONT)
-        
+        self.write(f"Level {self.level}", align="center", font=FONT)
+    
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over!", align="center", font=FONT)
     
