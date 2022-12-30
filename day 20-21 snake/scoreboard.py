ed8b1daa92c11f8ad2d4170e7593cdbc8c3bfb66
Benjy Lovat <benjy.lovat@gmail.com>
2022-12-30 09:24:12 +0000

    add increase score method else score increases when lose add reset method to reset score when lose Add high score attribute so high score displayed

--- day 20-21 snake/scoreboard.py
+++ day 20-21 snake/scoreboard.py
@@ -6,17 +6,27 @@ class Scoreboard(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.color("white")
         self.pu()
         self.ht() # hide turtle
         self.setpos(x=0, y=270)
-        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGN, font=FONT)
     
     def update_score(self):
-        self.score += 1
         self.clear() # clear writing every time score updates
-        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGN, font=FONT)
     
-    def game_over(self):
-        self.setpos(x=0, y=0)
-        self.write(f"GAME OVER!", align=ALIGN, font=FONT)
+    def increase_score(self):
+        self.score +=1
+        self.update_score()
+    
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score  
+        self.score=0
+        self.update_score()
+    
+    # def game_over(self):
+    #     self.setpos(x=0, y=0)
+    #     self.write(f"GAME OVER!", align=ALIGN, font=FONT)
