98277b833ec25c5f5a6d3668688bef26ee4464ca
benjyl <benjy.lovat@gmail.com>
2022-12-30 10:15:29 +0000

    load in saved previous high score

--- day 20-21 snake/scoreboard.py
+++ day 20-21 snake/scoreboard.py
@@ -1,32 +1,41 @@
 from turtle import Turtle
 ALIGN = "center"
-FONT =("arial", 20, "bold")
+FONT = ("arial", 20, "bold")
+
 
 class Scoreboard(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        self.highscore = self.get_prev_high_score()
         self.color("white")
         self.pu()
-        self.ht() # hide turtle
+        self.ht()  # hide turtle
         self.setpos(x=0, y=270)
-        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGN, font=FONT)
-    
+        self.write(
+            f"Score: {self.score} High Score: {self.highscore}", align=ALIGN, font=FONT)
+
+    def get_prev_high_score(self):
+        with open("data.txt") as data:
+            return int(data.read())
+
     def update_score(self):
-        self.clear() # clear writing every time score updates
-        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGN, font=FONT)
-    
+        self.clear()  # clear writing every time score updates
+        self.write(
+            f"Score: {self.score} High Score: {self.highscore}", align=ALIGN, font=FONT)
+
     def increase_score(self):
-        self.score +=1
+        self.score += 1
         self.update_score()
-    
+
     def reset(self):
         if self.score > self.highscore:
-            self.highscore = self.score  
-        self.score=0
+            self.highscore = self.score
+        self.score = 0
         self.update_score()
-    
+        with open("data.txt", mode='w') as data:
+            data.write(str(self.highscore))
+
     # def game_over(self):
     #     self.setpos(x=0, y=0)
     #     self.write(f"GAME OVER!", align=ALIGN, font=FONT)
