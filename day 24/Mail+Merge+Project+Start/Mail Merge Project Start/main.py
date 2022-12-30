750620c7f9b35feb0adbbcf6cff3008a4566eea3
benjyl <benjy.lovat@gmail.com>
2022-12-30 11:32:47 +0000

    create personalised letter complete

--- day 24/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ day 24/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -11,7 +11,17 @@
 invited = "/Users/benjy/OneDrive/Coding/100_day_code/day 24/Mail+Merge+Project+Start/Mail Merge Project Start/Input/Names/invited_names.txt"
 guest_list = [line.rstrip() for line in open(invited)]
 
-# letter =
-# with open
+letter = "/Users/benjy/OneDrive/Coding/100_day_code/day 24/Mail+Merge+Project+Start/Mail Merge Project Start/Input/Letters/starting_letter.txt"
+with open(letter, mode="r") as invitation:
+    invite = invitation.readlines()  # store generic invitation
 
-print(guest_list)
+
+# directory where want to store
+output_dir = "/Users/benjy/OneDrive/Coding/100_day_code/day 24/Mail+Merge+Project+Start/Mail Merge Project Start/Output/ReadyToSend/"
+
+# personalise invite for each guest
+for name in guest_list:
+    with open(f"{output_dir}letter_for_{name}.txt", mode="w") as invitation:
+        personal_invitation = [line.replace("[name]", name) for line in invite]
+        for line in personal_invitation:
+            invitation.write(line)
