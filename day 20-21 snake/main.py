5932ce36f1ce0a3aa338a1a8abb101f365fcbb50
benjyl <benjy.lovat@gmail.com>
2022-12-30 10:16:08 +0000

    remove spaces

--- day 20-21 snake/main.py
+++ day 20-21 snake/main.py
@@ -9,7 +9,7 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("SNAKE!!!!!!!!")
 
-screen.tracer(0) 
+screen.tracer(0)
 
 segments = []
 
@@ -17,10 +17,9 @@ sk = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
-
 screen.listen()
-screen.onkey(sk.up, "Up") # go up when click up arrow
-screen.onkey(sk.down,"Down")
+screen.onkey(sk.up, "Up")  # go up when click up arrow
+screen.onkey(sk.down, "Down")
 screen.onkey(sk.left, "Left")
 screen.onkey(sk.right, "Right")
 
@@ -28,27 +27,26 @@ screen.onkey(sk.right, "Right")
 game_on = True
 
 while game_on:
-    screen.update() # only updates once all segments have moved
+    screen.update()  # only updates once all segments have moved
     time.sleep(0.1)
     sk.move()
-    
-    
+
     # detect food collision
-    if food.distance((sk.head.pos())) <15:
-        food.move_pos() # move food to new position if eaten
+    if food.distance((sk.head.pos())) < 15:
+        food.move_pos()  # move food to new position if eaten
         sk.extend()
         scoreboard.increase_score()
         scoreboard.update_score()
-        
-    #Detect wall collision
-    if abs(sk.head.xcor()) >290 or abs(sk.head.ycor()) >290:
+
+    # Detect wall collision
+    if abs(sk.head.xcor()) > 290 or abs(sk.head.ycor()) > 290:
         scoreboard.reset()
         sk.reset()
         # game_on=False
     for segment in sk.segments[1:]:
-        if sk.head.distance(segment)<10:
+        if sk.head.distance(segment) < 10:
             scoreboard.reset()
             sk.reset()
-        
-    
+
+
 screen.exitonclick()
