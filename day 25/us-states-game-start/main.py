85ec777e7f3fdd89d3e52b7a5542a66700649632
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-03 12:31:01 +0000

    create state guesser

--- day 25/us-states-game-start/main.py
+++ day 25/us-states-game-start/main.py
@@ -1,7 +1,8 @@
 import turtle 
 import os
+import pandas as pd
 
-pwd = os.getcwd()
+pwd = os.getcwd()+"/day 25/us-states-game-start/"
 print(pwd)
 
 screen = turtle.Screen()
@@ -9,6 +10,10 @@ screen.title("U.S. States Game")
 image = "./day 25/us-states-game-start/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+pen = turtle.Turtle()
+pen.pu()
+pen.hideturtle()
 
 # # to get coords of point on screen when click mouse
 # def get_mouse_click_coor(x,y):
@@ -18,5 +23,27 @@ turtle.shape(image)
 
 # turtle.mainloop()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's the name of another state?")
-print(answer_state)
+score = 0 # keep track of user states guessed correctly 
+data = pd.read_csv(pwd+"50_states.csv")
+correct_guesses = []
+
+while len(correct_guesses) <50:   
+     
+    answer_state = screen.textinput(title=f"{len(correct_guesses)}/50 States guessed correctly", prompt="What's the name of another state?").title()
+    
+    if answer_state == "Exit":
+        break
+    
+    if answer_state in data["state"].unique() and answer_state not in correct_guesses: # could also turn data["state"] into list
+        x = int(data[data["state"] == answer_state].x) # x coord of state
+        y = int(data[data["state"] == answer_state].y) # y coord of state
+        pen.goto(x=x, y=y)
+        pen.write(answer_state)
+        correct_guesses.append(answer_state)
+        # print(correct_guesses)
+        
+    elif answer_state in data["state"].unique() and answer_state in correct_guesses:
+        print("already guessed")
+
+states_missed = data[~data.state.isin(correct_guesses)]
+states_missed.to_csv("./day 25/us-states-game-start/states_to_learn.csv")
