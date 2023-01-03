3dca3d938d2d647c4cf1c72869fcb6e642bb1842
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-03 11:06:26 +0000

    use pandas example

--- day 25/main.py
+++ day 25/main.py
@@ -1,16 +1,39 @@
 import os
 import csv
+import pandas
 
 path = os.getcwd()
-print(path)
+data =pandas.read_csv(path+"\\day 25\\weather_data.csv")
 
-with open(path+"\\day 25\\weather_data.csv") as file:
-    # data = file.read().splitlines()
-    data = csv.reader(file)
-    temps = []
-    for row in data:
-        if row[1].isnumeric():
-            temps.append(row[1])
-        
-print(temps)
-# print(data)
+data_dict = data.to_dict()
+print(data_dict)
+
+temps = data["temp"]
+
+# list mean
+temp_list = data["temp"].to_list()
+mean_temp = sum(temp_list)/len(temp_list)
+print(mean_temp)
+
+# pandas mean
+print(temps.mean())
+#max 
+max_temp = temps.max()
+# print row with max temp, condition
+print(data[data.temp == max_temp])
+
+#monday temp
+monday_temp = int(data[data.day == "Monday"].temp)
+monday_temp_fahr = monday_temp*1.8 + 32
+print(monday_temp_fahr)
+
+#create dataframe from scratch
+
+data_dict = {
+    "students": ["Amy", "James", "Benjy"],
+    "scores": [95, 83, 90]
+}
+
+student_dataframe = pandas.DataFrame(data_dict)
+student_dataframe.to_csv("test_data.csv")
+print(student_dataframe)
