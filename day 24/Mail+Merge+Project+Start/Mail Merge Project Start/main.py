fe1e717ccf2b1a72f3e3202104bae3606db09fed
benjyl <benjy.lovat@gmail.com>
2023-01-03 09:32:23 +0000

    removed instructions

--- day 24/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ day 24/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -1,12 +1,3 @@
-# TODO: Create a letter using starting_letter.txt
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
-
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
 # invited = "Input/Names/invited_names.txt" # relative path
 invited = "/Users/benjy/OneDrive/Coding/100_day_code/day 24/Mail+Merge+Project+Start/Mail Merge Project Start/Input/Names/invited_names.txt"
 guest_list = [line.rstrip() for line in open(invited)]
@@ -15,8 +6,7 @@ letter = "/Users/benjy/OneDrive/Coding/100_day_code/day 24/Mail+Merge+Project+St
 with open(letter, mode="r") as invitation:
     invite = invitation.readlines()  # store generic invitation
 
-
-# directory where want to store
+# directory where want to store personal invites
 output_dir = "/Users/benjy/OneDrive/Coding/100_day_code/day 24/Mail+Merge+Project+Start/Mail Merge Project Start/Output/ReadyToSend/"
 
 # personalise invite for each guest
