c636ffbf2b63519e531833d88c3e2d1d186189c1
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-06 12:38:47 +0000

    add args and kwargs to class

--- day 27 - Tkinter/playground.py
+++ day 27 - Tkinter/playground.py
@@ -17,3 +17,12 @@ sum_1 = add(1,3,5,6,3,1,3)
 sum_2 = add(15,3465,1223,124)
 
 calculate(2, add=3, multiply=5)
+
+class Car:
+    
+    def __init__(self, **kw) -> None:
+        self.make = kw.get("make")
+        self.model = kw.get("model") # returns none if not in dictionary
+        
+my_car = Car(make="Nissan", model="GTR")
+print(my_car.make)
