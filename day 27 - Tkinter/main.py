938106e6f67cd73997b8615fed1e6c2d58e12ead
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-06 12:38:35 +0000

    add button and text entry

--- day 27 - Tkinter/main.py
+++ day 27 - Tkinter/main.py
@@ -1,12 +1,32 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
 # label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack() # what creates label on screen - layout of component
+
+# methods for changing labels
+my_label["text"] = "New Text"
+my_label.config(text="Even newer text")
+
+def button_clicked():
+    output = input.get()
+    message = "clicked"
+    print(message)
+    my_label["text"] = output
+    
+
+input = Entry(width=10)
+input.pack()
+
+button = Button(text="Click me", command=button_clicked)
+button.pack()
+
+#Entry
+
 
 
 window.mainloop() # at end of script, everything else want to add in to window comes first
