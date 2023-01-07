7c047ae4c28732dc6bfd7bac74c205eabf762ac5
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-07 20:30:22 +0000

    move fuction to top of script

--- day 27 - Tkinter/main.py
+++ day 27 - Tkinter/main.py
@@ -1,32 +1,34 @@
 from tkinter import *
 
-window = Tk()
-window.title("My first GUI program")
-window.minsize(width=500, height=300)
-
-# label
-my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack() # what creates label on screen - layout of component
-
-# methods for changing labels
-my_label["text"] = "New Text"
-my_label.config(text="Even newer text")
-
 def button_clicked():
     output = input.get()
     message = "clicked"
     print(message)
     my_label["text"] = output
     
+window = Tk()
+window.title("My first GUI program")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=100)
+
+# label
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+# methods for changing labels
+my_label["text"] = "New Text"
+my_label.config(text="Even newer text")
+
+my_label.grid(row=0, column=0) # what creates label on screen - layout of component
+
+button = Button(text="Click me", command=button_clicked)
+button.grid(row=1, column=1)
+
+new_button = Button(text="Second click")
+new_button.grid(row=0, column =2)
+
 #Entry
 input = Entry(width=10)
-input.pack()
+input.grid(row=2, column=3)
  
-button = Button(text="Click me", command=button_clicked)
-button.pack()
-
-
-
 
 
 window.mainloop() # at end of script, everything else want to add in to window comes first
