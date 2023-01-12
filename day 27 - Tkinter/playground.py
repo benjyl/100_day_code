2a479caec9565e314dc5bbdd36cb4ed940939244
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-12 17:21:28 +0000

    format with black

--- day 27 - Tkinter/playground.py
+++ day 27 - Tkinter/playground.py
@@ -1,28 +1,32 @@
 # *args get passed in to function as a tuple
 # **kwargs passed in as a dict
 
+
 def add(*args):
     # total = 0
     # for i in args:
     #     total += i
     print(sum(args))
 
+
 def calculate(n, **kwargs):
     print(kwargs)
-    n+= kwargs["add"]
-    n*= kwargs["multiply"]
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
     print(n)
-    
-sum_1 = add(1,3,5,6,3,1,3)
-sum_2 = add(15,3465,1223,124)
+
+
+sum_1 = add(1, 3, 5, 6, 3, 1, 3)
+sum_2 = add(15, 3465, 1223, 124)
 
 calculate(2, add=3, multiply=5)
 
+
 class Car:
-    
     def __init__(self, **kw) -> None:
         self.make = kw.get("make")
-        self.model = kw.get("model") # returns none if not in dictionary
-        
+        self.model = kw.get("model")  # returns none if not in dictionary
+
+
 my_car = Car(make="Nissan", model="GTR")
 print(my_car.make)
