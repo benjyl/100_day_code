bcce3d66cf1fc28227f8d93e6ab0abc2255a00bf
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-13 08:35:57 +0000

    complete keyerror challenge

--- day 30 - Errors and Json/main..py
+++ day 30 - Errors and Json/main..py
@@ -43,3 +43,23 @@ def make_pie(index):
 
 make_pie(4)
 
+# Keyerror challenge: change code so posts with no likes key count as if 0 likes
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2}, 
+    {'Likes': 13, 'Comments': 2, 'Shares': 1}, 
+    {'Likes': 33, 'Comments': 8, 'Shares': 3}, 
+    {'Comments': 4, 'Shares': 2}, 
+    {'Comments': 1, 'Shares': 1}, 
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
+    try:
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        pass   
+
+print(total_likes)
