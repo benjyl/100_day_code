4016493646f1d0ad289ea74b12053a25b03e4095
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-13 08:48:19 +0000

    solution using while loop

--- day 30 - Errors and Json/nato_alph.py
+++ day 30 - Errors and Json/nato_alph.py
@@ -1,12 +1,21 @@
 import pandas
 
-# challenge: Create a script that will list the phonetic equivalent of a word
+# challenge: Create a script that will list the phonetic equivalent of a word and catch key errors
 phonetic_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 # {"A": "Alfa", "B": "Bravo"}
 phonetic_dict = {row.letter: row.code for (index, row) in phonetic_df.iterrows()}
 print(phonetic_dict)
 
 user_input = input("Input a word and receive the phonetic equivalent:\n").upper()
-word_list = [i for i in user_input]
-phonetic_list = [phonetic_dict[i] for i in user_input]
-print(phonetic_list)
+valid = False
+
+
+while not valid:
+    try:
+        phonetic_list = [phonetic_dict[i] for i in user_input]
+    except KeyError:
+        print("Sorry, only give letters in the alphabet please")
+        user_input = input("Input a word and receive the phonetic equivalent:\n").upper()
+    else:
+        print(phonetic_list)
+        valid = True
