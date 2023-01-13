1829e4d3d356d059f492a1895ccc8e5d3599ff86
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-13 08:49:42 +0000

    recursion exception handling

--- day 26 - dict and list comprehension/NATO-alphabet-start/main.py
+++ day 26 - dict and list comprehension/NATO-alphabet-start/main.py
@@ -1,3 +1,5 @@
+import pandas
+
 student_dict = {"student": ["Angela", "James", "Lily"], "score": [56, 76, 98]}
 
 # Looping through dictionaries:
@@ -5,7 +7,7 @@ for (key, value) in student_dict.items():
     # Access key and value
     pass
 
-import pandas
+
 
 student_data_frame = pandas.DataFrame(student_dict)
 
@@ -22,12 +24,11 @@ phonetic_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 # challenge: Create a script that will list the phonetic equivalent of a word
 
-# TODO 1. Create a dictionary in this format:
+
 # {"A": "Alfa", "B": "Bravo"}
 phonetic_dict = {row.letter: row.code for (index, row) in phonetic_df.iterrows()}
 print(phonetic_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 user_input = input("Input a word and receive the phonetic equivalent:\n").upper()
 word_list = [i for i in user_input]
 phonetic_list = [phonetic_dict[i] for i in user_input]
