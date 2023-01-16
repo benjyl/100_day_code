3b8f828b4872b7559fa4a9f1867101e2f16dc870
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-16 17:39:12 +0000

    implement english after 3s and french after click

--- day 31 - flashcards/main.py
+++ day 31 - flashcards/main.py
@@ -5,14 +5,29 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 #---------------------------------Reading csv------------------------
 data = pd.read_csv("./data/french_words.csv")
-data = data.to_dict("records")
+to_learn = data.to_dict("records")
 
 #------------------------------display new word----------------------
 def new_word():
-    word = random.choice(data)
+    
+    word = random.choice(to_learn) # gets random French word + translation
     front_word = word["French"]
-    canvas.itemconfig(canvas_title, text="French")
-    canvas.itemconfig(canvas_word, text=front_word)    
+    back_word = word["English"]
+    canvas.itemconfig(canvas_img, image=front_img)
+    canvas.itemconfig(canvas_title, text="French",font=("Arial", 40, "italic"), fill="black")
+    canvas.itemconfig(canvas_word, text=front_word, font=("Arial", 60, "bold"), fill="black")
+    reverse = canvas.after(3000, reverse_card, back_word)
+    # canvas.after_cancel(reverse)
+
+def reverse_card(english):
+    canvas.itemconfig(canvas_img, image=back_image)
+    # canvas_title = canvas.create_text(400, 150, text="English", font=("Arial", 40, "italic"), fill="white")
+    # canvas_word = canvas.create_text(400, 263, text=english, font=("Arial", 60, "bold"), fill="white")
+    canvas.itemconfig(canvas_title, text="English", fill="white")
+    canvas.itemconfig(canvas_word, text=english, fill="white")
+    
+    
+    
 
 #----------------------------------UI Setup--------------------------
 frame = Tk()
@@ -20,8 +35,9 @@ frame.title("Flash cards")
 frame.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-background_img = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=background_img)
+front_img = PhotoImage(file="./images/card_front.png")
+back_image = PhotoImage(file="./images/card_back.png")
+canvas_img = canvas.create_image(400, 263, image=front_img)
 
 # add text to canvas
 canvas_title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
@@ -37,5 +53,6 @@ tick_button.grid(column=1, row=1)
 cross = PhotoImage(file="./images/wrong.png")
 tick_button = Button(image=cross, bg=BACKGROUND_COLOR, command=new_word)
 tick_button.grid(column=0, row=1)
+
 
 frame.mainloop()
