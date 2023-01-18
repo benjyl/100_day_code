4aee3acff10ba323b92a6254fe69ffa4d98c0cb1
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-18 09:52:18 +0000

    finish flash card GUI

--- day 31 - flashcards/main.py
+++ day 31 - flashcards/main.py
@@ -6,8 +6,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 #---------------------------------Reading csv------------------------
 # check if words_to_learn file exists and if so, save to data otherwise use the french_words.csv
 try:
-    file = open("words_to_learn.csv")
-    data = pd.read_csv(file)
+    data = pd.read_csv("./data/words_to_learn.csv")
     to_learn = data.to_dict("records")
     print("Success")
 except FileNotFoundError:
@@ -17,7 +16,7 @@ except FileNotFoundError:
     
 current_card = {}
 #------------------------------display new word----------------------
-def new_word(known=False):
+def new_word():
     global current_card, flip_timer # allows me to update current card so stored for use in other functions
     
     frame.after_cancel(flip_timer)
@@ -27,8 +26,6 @@ def new_word(known=False):
     canvas.itemconfig(canvas_word, text=current_card["French"], font=("Arial", 60, "bold"), fill="black")
     #
     flip_timer = canvas.after(3000, func=reverse_card)
-    if known:
-        to_learn.remove(current_card)
 
 def reverse_card():
     # get error when start and wait more than 3s, no english in dictionary yet so check for key error and do nothing
@@ -40,6 +37,15 @@ def reverse_card():
         canvas.itemconfig(canvas_img, image=back_image)
         canvas.itemconfig(canvas_title, text="English", fill="white")
 
+def known():
+    try:
+        to_learn.remove(current_card)
+        data = pd.DataFrame(to_learn)
+        data.to_csv("./data/words_to_learn.csv", index=False, encoding = 'utf-8-sig')
+        new_word()
+    except(ValueError):
+        new_word()
+    
 #----------------------------------UI Setup--------------------------
 frame = Tk()
 frame.title("Flash cards")
@@ -60,7 +66,7 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 # tick button
 tick = PhotoImage(file="./images/right.png")
-tick_button = Button(image=tick, bg=BACKGROUND_COLOR, command=lambda: new_word(True))
+tick_button = Button(image=tick, bg=BACKGROUND_COLOR, command=known)
 tick_button.grid(column=1, row=1)
 
 # cross button
@@ -68,8 +74,9 @@ cross = PhotoImage(file="./images/wrong.png")
 tick_button = Button(image=cross, bg=BACKGROUND_COLOR, command=new_word)
 tick_button.grid(column=0, row=1)
 
+new_word() 
+
 
 frame.mainloop()
 
-to_learn_data = pd.DataFrame.from_dict(to_learn)
-to_learn_data.to_csv("words_to_learn.csv", index=False,  encoding = 'utf-8-sig') # encoding to stop accents on letters having issues
+
