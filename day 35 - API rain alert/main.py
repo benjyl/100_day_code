0750b927b1ce2ab191732f82a0f7b1b429cb9aea
Benjy Lovat <benjy.lovat@gmail.com>
2023-01-23 17:42:20 +0000

    add twilio for use in SMS

--- day 35 - API rain alert/main.py
+++ day 35 - API rain alert/main.py
@@ -1,6 +1,10 @@
 import requests
+from twilio.rest import Client
 
 api_key = "720dfad6d262378ab9f9e2a36c11e7eb"
+account_sid = "AC14cf7f3fe0c4ec44ec2132d3f678fd3e" # twilio account SID
+auth_token = "20cdb952026b26c67d1c5003bbda28a5" # twilio auth token
+client = Client(account_sid, auth_token)
 
 parameters={
     "lat": 51.5085,
@@ -15,8 +19,12 @@ hourly_weather = response.json()["hourly"][0:12]  # next 12 hours forecast
 
 weather_ids = [hourly_weather[i]["weather"][0]["id"] for i in range(len(hourly_weather))] # get weather ids for the next 12 hours
 if any(id < 700 for id in weather_ids):
-    print("bring a raincoat")
-else:
-    print("all clear")
+    message = client.messages \
+                .create(
+                     body="It's going to rain today. Bring a coat",
+                     from_='+15017122661', # change number to my temp number
+                     to='+447518778468'
+                )
+    print(message.status)
 
 
