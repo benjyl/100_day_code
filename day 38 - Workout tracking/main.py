250f225ffc9e3f1f2eb497eeab3318addc7be9ba
Benjy Lovat <benjy.lovat@gmail.com>
2023-02-01 14:13:47 +0000

    obtain exercise data from natural language. Save data to google sheets

--- day 38 - Workout tracking/main.py
+++ day 38 - Workout tracking/main.py
@@ -1,4 +1,45 @@
 import requests
+from requests.auth import HTTPBasicAuth
+from datetime import datetime
 
 APP_ID = "adbce002"
 API_KEY = "27e49ce435ee2dd908ae1a13754df38a"
+
+USERNAME = "benjyl64"
+PASSWORD = "SDF^£Fdsdaf4634@ASed"
+
+nutrition_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+parameters = {
+    "query": input("What exercise did you do today?")
+}
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
+}
+response = requests.post(url=nutrition_endpoint, json=parameters, headers=headers)
+response_data = response.json()
+
+# make sure sheet is not in read only mode
+sheety_endpoint = 'https://api.sheety.co/b2bfc39b396fc0ac0f35c7b4b25d0fc6/myWorkouts/workouts'
+
+# basic 
+
+for ex in response_data["exercises"]:
+    # print(ex["user_input"], ex["duration_min"],ex["nf_calories"])
+    sheet_inputs = {
+        "workout":{
+            "date": datetime.today().strftime('%d/%m/%Y'),
+            "time": datetime.today().strftime("%H:%M:%S"),
+            "exercise": ex["user_input"].title(),
+            "duration": ex["duration_min"],
+            "calories": ex["nf_calories"]
+        }
+    }
+    sheets_response = requests.post(url=sheety_endpoint, json=sheet_inputs)
+    print(sheets_response.text)
+    sheets_response.raise_for_status()
+
+
+# print(response.text)
