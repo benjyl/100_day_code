becfc9e424c579d3e8a08e6ff284e49b8df964db
Benjy Lovat <benjy.lovat@gmail.com>
2023-02-07 09:13:44 +0000

    add flight finder and test mode

--- day 39 - flight price tracker/main.py
+++ day 39 - flight price tracker/main.py
@@ -8,16 +8,20 @@ sheet_data = data_manager.get_sheet_data()
 print(sheet_data)
 
 add_iata = False # checks if IATA needs to be added to sheet data
-
-for city in sheet_data:
-    print(city)
-    if city["iataCode"] == "":
-        add_iata = True
-        iata_code = flight_searcher.get_IATA_code(city["city"])
-        city["iataCode"] = iata_code
+if sheet_data:
+    for city in sheet_data:
+        print(city)
+        if city["iataCode"] == "":
+            add_iata = True
+            iata_code = flight_searcher.get_IATA_code(city["city"])
+            city["iataCode"] = iata_code
         
 if add_iata:
     data_manager.flight_limits = sheet_data        
     data_manager.add_iata()
+
+cities = ["PAR", "BER", "TYO", "SYD", "IST", "KUL", "NYC", "SFO", "CPT"]
+flight_data = flight_searcher.find_flight(cities)
+print(flight_data["data"])
 
 
