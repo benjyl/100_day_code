f71880c7138518137e27250515a81a7d857464c7
Benjy Lovat <benjy.lovat@gmail.com>
2023-02-07 14:30:28 +0000

    reformat

--- day 39 - flight price tracker/flight_search.py
+++ day 39 - flight price tracker/flight_search.py
@@ -1,65 +1,61 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
-FLIGHT_API ="c2hvy6LmveKkQx7e0-p1z-HQG_V9k_9B"
-IATA_ENDPOINT = "https://api.tequila.kiwi.com/locations/query" 
+FLIGHT_API = "c2hvy6LmveKkQx7e0-p1z-HQG_V9k_9B"
+IATA_ENDPOINT = "https://api.tequila.kiwi.com/locations/query"
 FLIGHT_SEARCH_ENDPOINT = "https://api.tequila.kiwi.com/v2/search"
 
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
+    # This class is responsible for talking to the Flight Search API.
     def __init__(self) -> None:
         pass
-    
+
     def get_IATA_code(self, city):
         # input city and look for top city given, the city code is the IATA code
-        params={
-            "term": city,
-            "location_types": "city",
-            "limit": 1
-        }
-        headers = {
-            "apikey": FLIGHT_API
-        }
-        
-        flight_response = requests.get(url=IATA_ENDPOINT, params=params, headers=headers)
+        params = {"term": city, "location_types": "city", "limit": 1}
+        headers = {"apikey": FLIGHT_API}
+
+        flight_response = requests.get(
+            url=IATA_ENDPOINT, params=params, headers=headers
+        )
         flight_response.raise_for_status()
         flight_response_data = flight_response.json()
         iata_code = flight_response_data["locations"][0]["code"]
         return iata_code
-    
-    def find_flight(self, cities):
+
+    def find_flight(self, cities_data):
+        cities = [cities_data[i]["iataCode"] for i in range(len(cities_data))]
         cities = ",".join(cities)
         print(cities)
-        today = datetime.now().strftime("%d/%m/%Y")
-        six_months = datetime.now()+relativedelta(months=6)
-        six_months = six_months.strftime("%d/%m/%Y")
-        print(six_months)
-        headers = {
-            "apikey": FLIGHT_API
-        }
+        today = datetime.now()
+        tomorrow = (today + timedelta(days=1)).strftime("%d/%m/%Y")
+        print(tomorrow)
+        next_year = (datetime.now() + relativedelta(years=1)).strftime("%d/%m/%Y")
+        print(next_year)
+        headers = {"apikey": FLIGHT_API}
         params = {
             "fly_from": "LON",
             "fly_to": cities,
-            "date_from": today,
-            "date_to": six_months,
+            "date_from": tomorrow,
+            "date_to": next_year,
             "nights_in_dst_from": 2,
-            "nights_in_dst_to": 7,
+            "nights_in_dst_to": 28,
             "flight_type": "round",
             "one_for_city": 1,
             "ret_from_diff_city": "False",
-            "ret_to_diff_city":"False",
+            "ret_to_diff_city": "False",
             "selected_cabins": "M",
             "adult_hold_bag": "1",
             "adult_hand_bag": "1",
             "curr": "GBP",
-            "max_stopovers":1,
-            "limit":10
+            "limit": 30,
         }
-        flight_response = requests.get(url=FLIGHT_SEARCH_ENDPOINT, params=params, headers=headers)
+        flight_response = requests.get(
+            url=FLIGHT_SEARCH_ENDPOINT, params=params, headers=headers
+        )
+        print()
         flight_response.raise_for_status()
-        flight_response_data = flight_response.json()
+        flight_response_data = flight_response.json()["data"]
         return flight_response_data
-       
-
-    
