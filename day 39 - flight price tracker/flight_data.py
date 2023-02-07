39fb474420a2ad7d628d844ddc8758a3dd91be95
Benjy Lovat <benjy.lovat@gmail.com>
2023-02-07 14:26:43 +0000

    add sort flight data method

--- day 39 - flight price tracker/flight_data.py
+++ day 39 - flight price tracker/flight_data.py
@@ -1,3 +1,17 @@
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    # This class is responsible for structuring the flight data.
+    def __init__(self) -> None:
+        self.city_prices = {}
+
+    def sort_flight_data(self, flight_data):
+        # print(self.city_prices)
+        for flight in flight_data:
+            print(flight["cityTo"])
+            self.city_prices[f"{flight['cityTo']}"] = {
+                "from": f"{flight['cityFrom']}-{flight['flyFrom']}",
+                "to": f"{flight['cityTo']}-{flight['flyTo']}",
+                "price": flight["price"],
+                "date_from": flight["route"][0]["local_arrival"][:10],
+                "date_to": flight["route"][1]["local_arrival"][:10],
+            }
+        return self.city_prices
