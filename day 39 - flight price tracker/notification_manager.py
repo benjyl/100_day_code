2dce12612d0b895166fbfb3ccfbb67067630cf6f
Benjy Lovat <benjy.lovat@gmail.com>
2023-02-09 12:34:54 +0000

    add method to email everyone in database. Retry once new month - used up all sheety attempts for month

--- day 39 - flight price tracker/notification_manager.py
+++ day 39 - flight price tracker/notification_manager.py
@@ -1,10 +1,15 @@
 from twilio.rest import Client
+import requests
+import smtplib
+from email.mime.text import MIMEText
 
 TWILIO_SID = "AC14cf7f3fe0c4ec44ec2132d3f678fd3e"
 AUTH_TOKEN = "20cdb952026b26c67d1c5003bbda28a5"
 TWILIO_PHONE = "+19137330970"
 MY_PHONE = "+447518778468"
-
+MY_EMAIL = "jstew613@gmail.com"
+PASSWORD = "ioylffhmzmwwrmnq"
+SHEETY_USERS_ENDPOINT = "https://api.sheety.co/b2bfc39b396fc0ac0f35c7b4b25d0fc6/myFlightDeals/users"
 
 class NotificationManager:
     """This class is responsible for sending notifications with the deal flight details."""
@@ -37,14 +42,17 @@ class NotificationManager:
                     #     f"Low price alert! Only £{round(flight_price)} to fly from {flight_data[city]['from']} \
                     #       to {flight_data[city]['to']} from {flight_data[city]['date_from']} to {flight_data[city]['date_to']}"
                     # )
-                    client = Client(TWILIO_SID, AUTH_TOKEN)
-                    message = client.messages.create(
-                        body=f"Low price alert! Only £{round(flight_price)} to fly from {flight_data[city]['from']} \
-                            to {flight_data[city]['to']} from {flight_data[city]['date_from']} to {flight_data[city]['date_to']}",
-                        from_=TWILIO_PHONE,
-                        to=MY_PHONE,
-                    )
+                    print("found a deal")
+                    # client = Client(TWILIO_SID, AUTH_TOKEN)
+                    # message = client.messages.create(
+                    #     body=f"Low price alert! Only £{round(flight_price)} to fly from {flight_data[city]['from']} \
+                    #         to {flight_data[city]['to']} from {flight_data[city]['date_from']} to {flight_data[city]['date_to']}",
+                    #     from_=TWILIO_PHONE,
+                    #     to=MY_PHONE,
+                    # )
                     good_deal = True
+                    self.email_customers(city, flight_price, flight_data)
+                    
         if not good_deal:
             print("No good deal")
             # client = Client(TWILIO_SID, AUTH_TOKEN)
@@ -53,3 +61,20 @@ class NotificationManager:
             #     from_=TWILIO_PHONE,
             #     to=MY_PHONE,
             # )
+    
+    def email_customers(self, city, flight_price, flight_data):
+        # response = requests.get(url=SHEETY_USERS_ENDPOINT).json()
+        # print(response)
+        # emails = [response["users"][i]["email"] for i in range(len(response))]
+        emails = ["benjy.lovat@gmail.com"]
+        message = f"Low price alert! Only £{round(flight_price)} to fly from {flight_data[city]['from']}o {flight_data[city]['to']} from {flight_data[city]['date_from']} to {flight_data[city]['date_to']}\n{flight_data[city]['URL']}"
+        
+        for email in emails:
+            message = MIMEText(message)
+            message["subject"] = "Flight deal alert"
+            message["from"] = MY_EMAIL
+            message["to"] = email
+            with smtplib.SMTP("smtp.gmail.com", 587) as connection:  # 587 - port number to successfully connect for email
+                connection.starttls()  # tls (transport layer security-secure connection to email server)
+                connection.login(user=MY_EMAIL, password=PASSWORD)
+                connection.sendmail(from_addr=MY_EMAIL, to_addrs=email, msg=message.as_string())
