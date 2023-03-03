b26119406e72764d1a60f7226417133839e9e790
Benjy Lovat <benjy.lovat@gmail.com>
2023-03-03 09:58:08 +0000

    add function to update item cost. Click on highest cost available item every 5 seconds

--- day 48 - selenium webdriver/cookie_click.py
+++ day 48 - selenium webdriver/cookie_click.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys # replicates key that's not letter, number or symbols, e.g. enter key
 from time import time
-import re
+from bisect import bisect
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -15,19 +15,37 @@ cookie = driver.find_element(By.ID, "cookie")
 start_time = time()
 five_sec_start = start_time
 
+def get_shop_options():
+    """get shop items for clicking on and costs
+    item cost increases every time by a new one
 
-shop = driver.find_elements(By.CSS_SELECTOR, "#store div b")
+    Returns:
+        list, list: list of shop items and costs
+    """
+    shop = driver.find_elements(By.CSS_SELECTOR, "#store div b")
+    items = [item.text for item in shop]
+    costs = []
+    for item in items:
+        split_item = item.split(" - ")
+        # print(split_item)
+        try:
+            costs.append(int(split_item[1].replace(",", "")))
+        except IndexError:
+            pass
+    return shop, costs
 
-items = [item.text for item in shop]
-for item in items:
-    split_item = item.split(" - ")
-    print(split_item)
 
-
-while time() - start_time < 20:
+while time() - start_time < 50:
     while time() - five_sec_start <5:
         cookie.click()
-    shop = driver.find_elements(By.CSS_SELECTOR, "#store div")
-    shop[1].click()
-    # print([item.text for item in shop])
+    money = int(driver.find_element(By.ID, "money").text)
+    shop, costs = get_shop_options()
+    # print(costs)
+    partition_index = bisect(costs, money)
+    shop[partition_index-1].click()
     five_sec_start = time()
+
+cookies_second = driver.find_element(By.ID, "cps").text
+print(cookies_second)
+
+driver.quit()
