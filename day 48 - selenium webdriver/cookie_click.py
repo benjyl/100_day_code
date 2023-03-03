dc55371f517bc59d7561f8888a4b757207f8f2ad
Benjy Lovat <benjy.lovat@gmail.com>
2023-03-03 09:27:30 +0000

    find items and click on 2nd one every five seconds

--- day 48 - selenium webdriver/cookie_click.py
+++ day 48 - selenium webdriver/cookie_click.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys # replicates key that's not letter, number or symbols, e.g. enter key
 from time import time
+import re
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -14,7 +15,19 @@ cookie = driver.find_element(By.ID, "cookie")
 start_time = time()
 five_sec_start = start_time
 
-while time() - start_time < 300:
+
+shop = driver.find_elements(By.CSS_SELECTOR, "#store div b")
+
+items = [item.text for item in shop]
+for item in items:
+    split_item = item.split(" - ")
+    print(split_item)
+
+
+while time() - start_time < 20:
     while time() - five_sec_start <5:
         cookie.click()
+    shop = driver.find_elements(By.CSS_SELECTOR, "#store div")
+    shop[1].click()
+    # print([item.text for item in shop])
     five_sec_start = time()
