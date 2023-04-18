8eb2e5fb3080ca0d2b4a2ef2fe447918fadd49f1
Benjy Lovat <benjy.lovat@gmail.com>
2023-04-18 08:42:10 +0100

    add comments

--- day 54 - intro to flask/hello.py
+++ day 54 - intro to flask/hello.py
@@ -1,9 +1,18 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # Flask class
 
-@app.route("/")
+print(__name__)
+
+#decorator adds additional functionality to existing function
+
+@app.route("/") # when user navigates to our URL with just / show how page
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "Hello, World!"
 
+if __name__ == "__main__":
+    app.run()
 # flask --app hello run - run this in command line to get flask to work
+
+# Python decorator function
+
