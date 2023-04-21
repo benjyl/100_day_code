ef17393b26255e1691b50be2a912bf1a55ce5673
benjyl <benjy.lovat@gmail.com>
2023-04-21 18:09:43 +0100

    add API for age and gender guess

--- day 57 - Jinja/server.py
+++ day 57 - Jinja/server.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template
 from datetime import date
+import requests
 
 app = Flask(__name__)
+
+
+def guess_age(name):
+    age_params = {"name": name}
+    response = requests.get("https://api.agify.io", params=age_params)
+    response.raise_for_status()
+    data = response.json()
+    age = data["age"]
+    return age
+
+
+def guess_gender(name):
+    params = {"name": name}
+    response = requests.get("https://api.genderize.io", params=params)
+    response.raise_for_status()
+    data = response.json()
+    gender = data["gender"]
+    return gender
 
 
 @app.route("/")
@@ -10,6 +29,16 @@ def home():
     # for every python variable want to include in the html, add another keyword argument
     # Each one needs name and value specified, can't just hand in the variable
     return render_template("index.html", year=curr_year)
+
+
+@app.route("/guess/<name>")
+def guess(name):
+    # Get guess from API of user age
+    name = name.capitalize()
+    age = guess_age(name)
+    gender = guess_gender(name)
+
+    return render_template("guess.html", name=name, gender=gender, age=age)
 
 
 if __name__ == "__main__":
