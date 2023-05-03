00eb76ee3e4535bc5ddbf95238eb2eda6ee3a4bb
Benjy Lovat <benjy.lovat@gmail.com>
2023-05-03 11:57:16 +0100

    add functions for different pages

--- day 59 - blog capstone/main.py
+++ day 59 - blog capstone/main.py
@@ -3,8 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def home():
     return render_template("index.html")
+
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact.html')
+def contact_me():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
