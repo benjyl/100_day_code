c00bc07e00c13d22394410e3f367f3a92d9e68ac
Benjy Lovat <benjy.lovat@gmail.com>
2023-05-03 13:27:03 +0100

    fetch and render blog posts from API

--- day 59 - blog capstone/main.py
+++ day 59 - blog capstone/main.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+import requests
+from datetime import date, timedelta
 
 app = Flask(__name__)
+
+def get_blog_data():
+    response = requests.get("https://api.npoint.io/0f66887cfe209d777703")
+    response.raise_for_status()
+    data = response.json()
+    return data
 
 @app.route('/')
 @app.route('/index.html')
 def home():
-    return render_template("index.html")
+    blog_data = get_blog_data()
+    for i in range (len(blog_data)):
+        blog_data[i]["date"] = str(date.today() - timedelta(days=i*7))
+    return render_template("index.html", data=blog_data)
 
 @app.route('/about.html')
 def about():
@@ -16,4 +27,10 @@ def contact_me():
     return render_template("contact.html")
 
 if __name__ == "__main__":
+    blog_data = get_blog_data()
+    for i in range (len(blog_data)):
+        blog_data[i]["date"] = str(date.today() - timedelta(days=i*7))
+    print(blog_data)
+    print(len(get_blog_data()))
+    print(date.today())
     app.run(debug=True)
