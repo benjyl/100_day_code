e33b6d44d0a0177a87109ed6bfa0c7d597d22598
Benjy Lovat <benjy.lovat@gmail.com>
2023-05-05 09:51:09 +0100

    Update contact page header with sent message

--- day 59 - blog capstone/main.py
+++ day 59 - blog capstone/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from datetime import date, timedelta
 
@@ -17,14 +17,27 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route('/contact')
-def contact_me():
-    return render_template("contact.html")
 
 @app.route('/post/<int:num>')
 def post(num):
     # blog_post = 
     return render_template('post.html', post_data=blog_data[num-1])
+
+@app.route("/contact", methods=["GET", "POST"])
+def receive_data():
+    # If click on contact button / type url /contact
+    if request.method == "GET":
+        return render_template("contact.html")
+    # If press submit button on form
+    else:
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+        return render_template("contact.html", sent=True)
+        
+        
+        return f"<h1>Name: {name} <br/> email: {email} <br/> phone: {phone} <br/> message: {message} <br/>Successfully sent your message"
 
 if __name__ == "__main__":
     app.run(debug=True)
