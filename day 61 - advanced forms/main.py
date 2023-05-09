44c0678a4b2f76322a603bded08fb6a31e0951b4
benjyl <benjy.lovat@gmail.com>
2023-05-09 10:06:20 +0100

    render homepage

--- day 61 - advanced forms/main.py
+++ day 61 - advanced forms/main.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+
+class LoginForm(FlaskForm):
+    pass
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/login")
+def login():
+    form = FlaskForm()
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
