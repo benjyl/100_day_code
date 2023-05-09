cf7ffb98b9485a55c995356fda3e44822cbb27f0
Benjy Lovat <benjy.lovat@gmail.com>
2023-05-09 10:39:30 +0100

    Add multiple wtform field types for form

--- day 61 - advanced forms/main.py
+++ day 61 - advanced forms/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField,PasswordField, BooleanField, SubmitField
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -8,9 +8,10 @@ app.secret_key = "sdfsertsd452&"
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email")
-    password = StringField("Password")
-
+    email = StringField(label="Email")
+    password = PasswordField(label="Password")
+    terms = BooleanField(label="I agree to T&Cs")
+    submit = SubmitField(label="Log In")
 
 @app.route("/")
 def home():
