4a77de0ae169fb0ffa296aa20d4efd6a055de5fb
Benjy Lovat <benjy.lovat@gmail.com>
2023-05-17 14:59:21 +0100

    go through basic SQLAlchemy

--- day 63 - SQLite intro/main.py
+++ day 63 - SQLite intro/main.py
@@ -1,7 +1,9 @@
 import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor() # cursor to control/modify database, e.g. add data row or edit/delete data
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor() # cursor to control/modify database, e.g. add data row or edit/delete data
 
 # .execute - tells cursor to execute an action
 # CREATE TABLE - creates new database table - table name comes after keyword
@@ -12,7 +14,44 @@ cursor = db.cursor() # cursor to control/modify database, e.g. add data row or e
 # NOTNULL - must have a value (can't be empty) UNIQUE - no 2 records can have the same title
 # author varchar(250) NOT NULL - field accept string up to 250 chars, cannot be empty
 # rating FLOAT NOT NULL- field accepts float cannot be empty
+
 # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J.K. Rowling', '9.3')")
-db.commit() # add the entry
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J.K. Rowling', '9.3')")
+# db.commit() # add the entry
+
+# Using Flask alchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    
+    def __repr__(self):
+        return f'<Book {self.title}>'
+    
+with app.app_context():
+    db.create_all()
+    new_book = Book(id=1, title="Harry Potter", author="J.K. Rowling", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
+    all_books = db.session.query(Book).all
+    print(all_books())
+    
+    book = Book.query.filter_by(title="Harry Potter").first()
+    book_to_update = Book.query.filter_by(title="Harry Potter").first()
+    book_to_update.title = "Harry Potter and the Chamber of Secrets"
+    db.session.commit()
+    book_id = 1
+    print(book_to_update)
+    
+    book_to_delete = db.session.get(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    
+    
