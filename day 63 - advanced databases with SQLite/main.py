5c83f7debc53a9b999461816d138acbee785c545
Benjy Lovat <benjy.lovat@gmail.com>
2023-05-31 15:26:58 +0100

    add SQL database book storage

--- day 63 - advanced databases with SQLite/main.py
+++ day 63 - advanced databases with SQLite/main.py
@@ -1,20 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-all_books = []
-
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all() # get all books in session
     return render_template("index.html",  books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        all_books.append(request.form.to_dict())
-        print(all_books)
+        new_book = Book(title=request.form["title"], author=request.form["author"], rating=request.form["rating"])
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
 
