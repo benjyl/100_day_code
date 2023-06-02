09d2dc14a38e20682f594ad2865a16cb09adb7dd
Benjy Lovat <benjy.lovat@gmail.com>
2023-06-02 08:35:27 +0100

    initial commit - add movie class for sql

---
+++ day 64 -top movies site/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Top-10-movies.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+Bootstrap(app)
+
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(1000), unique=True, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+with app.app_context():
+    db.create_all()
+    if not Movie.query.first():
+        new_movie = Movie(title="Phone Booth", year=2002,
+        description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+        rating = 7.3, ranking=10, review="My favourite character was the caller",
+        img_url = "https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg")
+        db.session.add(new_movie)
+        db.session.commit()
+    else:
+        print(Movie.query.first())
+
+@app.route("/")
+def home():
+    top_movies = Movie.query.all()
+    return render_template("index.html", movies = top_movies)
+
+
+if __name__ == '__main__':
+    
+    app.run(debug=True)
